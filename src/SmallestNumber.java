954983957c8b018680c4a99429cb3f2846317fa0
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-06 23:28:44 +0530

    Add optimal approach for SmallestNumber.java

--- src/SmallestNumber.java
+++ src/SmallestNumber.java
@@ -1,14 +1,27 @@
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class SmallestNumber {
+
+//    Time Complexity: O(N log N), where N is the size of the array, as we are sorting the array.
+//    Space Complexity: O(1), as we are using a constant
     public static int sort(int[] nums) {
         int n = nums.length;
         Arrays.sort(nums);
         return nums[0];
     }
+
+    public static int smallest(int[] nums){
+        int small = nums[0];
+        for(int num : nums) {
+            if(num < small) {
+                small = num;
+            }
+        }
+        return small;
+    }
     public static void main(String[] args) {
         int[] nums = {2,5,6,1,-1};
         System.out.println("Smallest number in an array is : " + sort(nums));
+        System.out.println("Smallest number : " + smallest(nums));
     }
 }
