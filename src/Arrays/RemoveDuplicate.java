d12e9dfb07f77ec607fce06e9be0b0abbc6354b1
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-07 22:12:00 +0530

    RemoveDuplicate.java

---
+++ src/Arrays/RemoveDuplicate.java
@@ -0,0 +1,38 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class RemoveDuplicate {
+    public static int remove(int[] nums) {
+        int n = nums.length;
+//        HashSet<Integer> seen = new HashSet<>();
+//        int idx = 0;
+//        for(int num : nums) {
+//            if(!seen.contains(num)) {
+//                seen.add(num);
+//                nums[idx] = num;
+//                idx++;
+//            }
+//        }
+//        return idx;
+
+        int idx = 0;
+        if(n == 0) return 0;
+        for(int i = 0; i < n; i++) {
+            if(nums[i] != nums[idx]) {
+                idx++;
+                nums[idx] = nums[i];
+            }
+        }
+        return idx + 1;
+
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,1,2,2,3,3};
+        int res = remove(nums);
+        System.out.println("Array after removing duplicate : ");
+        for(int i = 0; i < res; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
