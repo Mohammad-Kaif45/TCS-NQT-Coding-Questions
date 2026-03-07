2eb0294da2611f8be76a1c8d0e25e24be6cd7563
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-07 09:18:13 +0530

    Reverse An Array

--- src/Arrays/Reverse.java
+++ src/Arrays/Reverse.java
@@ -1,10 +1,18 @@
 package Arrays;
 
-public class Reverse {
-    public static void reverse(int[] nums) {
+import java.util.Arrays;
 
+public class Reverse {
+    public static int[] reverse(int[] nums) {
+        int[] rev = new int[nums.length];
+        int idx = 0;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            rev[idx++] = nums[i];
+        }
+        return rev;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,5};
+        System.out.println(Arrays.toString(reverse(nums)));
     }
 }
