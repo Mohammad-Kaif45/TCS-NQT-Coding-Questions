6bf4ecb83cd98a4df015da8c8b7dc6c77138cff6
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-09 16:24:10 +0530

    Adding Element in an Array

---
+++ src/Arrays/AddElementAtBeginning.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class AddElementAtBeginning {
+    public static int[] insertAtStart(int[] nums,int ele) {
+        int[] newArr = new int[nums.length + 1];
+        newArr[0] = ele;
+        for(int i = 0; i < nums.length; i++) {
+            newArr[i + 1] = nums[i];
+        }
+        return newArr;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,3,4,5};
+        System.out.println(Arrays.toString(insertAtStart(nums,1)));
+    }
+}
