d69ef5f9600f83e2f3e67abddf9a297fbb166b2a
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-09 16:35:05 +0530

    Adding Element in an Array at end

--- src/Arrays/AddElementAtBeginning.java
+++ src/Arrays/AddElementAtBeginning.java
@@ -11,8 +11,18 @@ public class AddElementAtBeginning {
         }
         return newArr;
     }
+    public static int[] insertAtEnd(int[] nums,int ele) {
+        int[] newArr = new int[nums.length + 1];
+
+        for(int i = 0; i < nums.length; i++) {
+            newArr[i] = nums[i];
+        }
+        newArr[nums.length] = ele;
+        return newArr;
+    }
     public static void main(String[] args) {
         int[] nums = {2,3,4,5};
-        System.out.println(Arrays.toString(insertAtStart(nums,1)));
+//        System.out.println(Arrays.toString(insertAtStart(nums,1)));
+        System.out.println(Arrays.toString(insertAtEnd(nums,6)));
     }
 }
