197ce82328c2a31fc8a98a5e50bf9b2f186d6268
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-11 12:41:32 +0530

    Given an array of pairs, find all the symmetric pairs in the array. (Optimal Approach add)

--- src/Arrays/SymmetricPair.java
+++ src/Arrays/SymmetricPair.java
@@ -1,5 +1,5 @@
 package Arrays;
-
+import java.util.*;
 public class SymmetricPair {
 
     // Brute Force using two loop
@@ -13,8 +13,29 @@ public class SymmetricPair {
             }
         }
     }
+
+    public void findSymmetricPairs(int[][] arr) {
+        Map<Integer, Integer> mp = new HashMap<>();  // Map to store first element and its pair
+        System.out.println("The Symmetric Pairs are:");
+
+        // Loop through the array to find symmetric pairs
+        for (int i = 0; i < arr.length; i++) {
+            int first = arr[i][0];
+            int second = arr[i][1];
+
+            // Check if {second, first} existed previously
+            if (mp.containsKey(second) && mp.get(second) == first) {
+                System.out.print("(" + first + " " + second + ") ");
+            } else {
+                // Store {first, second} pair in the map
+                mp.put(first, second);
+            }
+        }
+    }
     public static void main(String[] args) {
         int[][] pairs = {{1, 2}, {2, 1}, {3, 4}, {4, 5}, {5, 4}};
         checkSymmetry(pairs);
+        SymmetricPair obj = new SymmetricPair();
+        obj.findSymmetricPairs(pairs);
     }
 }
