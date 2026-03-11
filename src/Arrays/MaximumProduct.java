30b3c9e7c58446a2cc4bd150d50d38a2f088c4ff
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-11 12:51:19 +0530

    Maximum Product Subarray in an Array

---
+++ src/Arrays/MaximumProduct.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class MaximumProduct {
+    public static int maxproduct(int[] nums) {
+        int max = nums[0];
+        for(int i = 0; i < nums.length; i++) {
+            int prod = 1;
+            for(int j = i; j < nums.length; j++) {
+                prod = prod * nums[j];
+                max = Math.max(prod,max);
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,0};
+        System.out.println(maxproduct(nums));
+    }
+}
