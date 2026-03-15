18baa33136147fd2fb7c0d4d99a0cc93623543b8
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-15 14:49:13 +0530

    Two Sum

--- src/Arrays/TwoSum.java
+++ src/Arrays/TwoSum.java
@@ -1,16 +1,16 @@
 package Arrays;
 import java.util.*;
 public class TwoSum {
-    public static boolean twoSum(int[] nums,int target) {
+    public static int[] twoSum(int[] nums,int target) {
         int n = nums.length;
         for(int i = 0; i < n; i++) {
             for(int j = i + 1; j< n; j++) {
                 if(nums[i] + nums[j] == target) {
-                    return true;
+                    return new int[] {i,j};
                 }
             }
         }
-        return false;
+        return new int[] {-1,-1};
     }
 
     public int[] twoSumIndices(int[] arr, int target) {
@@ -29,7 +29,7 @@ public class TwoSum {
     }
     public static void main(String[] args) {
         int[] nums = {-1,1,3,4,6};
-        System.out.println(twoSum(nums,6));
+        System.out.println(Arrays.toString(twoSum(nums,0)));
         TwoSum obj = new TwoSum();
         System.out.println(Arrays.toString(obj.twoSumIndices(nums,5)));
     }
