5bcd2736c33dab8ecfb2c6279912905e3c2b8c3a
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-03-26 16:05:30 +0530

    Max profit

---
+++ src/Arrays/BuyAndSell.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class BuyAndSell {
+    public static int maxProfit(int[] nums) {
+        int n = nums.length;
+        int profit = 0;
+        int buy = nums[0];
+        for(int i = 0; i < n; i++) {
+            if(nums[i] < buy) {
+                buy = nums[i];
+            } else if(nums[i] - buy > profit) {
+                profit = nums[i] - buy;
+            }
+        }
+        return profit;
+    }
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        System.out.println("Maximum profit is : " + maxProfit(prices));
+    }
+}
