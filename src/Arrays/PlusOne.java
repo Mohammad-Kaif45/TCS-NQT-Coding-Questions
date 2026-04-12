9ce7cfbe886f13fe613e563fd376142afcfc8d01
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-04-12 17:18:08 +0530

    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
    Increment the large integer by one and return the resulting array of digits.

--- src/Arrays/PlusOne.java
+++ src/Arrays/PlusOne.java
@@ -11,12 +11,13 @@ public class PlusOne {
                 return nums;
             }
             nums[i] = 0;
-
         }
         int[] result = new int[n + 1];
         nums[0] = 1;
         return result;
     }
+
+    // Main method
     public static void main(String[] args) {
         int[] nums = {1,0,0};
         PlusOne obj = new PlusOne();
