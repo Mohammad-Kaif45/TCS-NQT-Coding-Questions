bed8a909009d7bb2972d3cf79aad0b8de55bad62
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-04-25 11:11:30 +0530

    Product of array except itself

---
+++ src/Arrays/Product.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class Product {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int product = 1;
+        for(int num : nums) {
+            product = product * num;
+        }
+        for(int i = 0; i < n; i++) {
+            ans[i] = product / nums[i];
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        Product obj = new Product();
+        int[] result = obj.productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
