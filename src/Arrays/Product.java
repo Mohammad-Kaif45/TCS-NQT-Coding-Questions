6721a598dbe1ed95c43376d569cc131524905353
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-04-27 22:10:52 +0530

    update

--- src/Arrays/Product.java
+++ src/Arrays/Product.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Product {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] ans = new int[n];
+        int[] ans = new int[n]; // create a answer array whose length is same as nums
         int product = 1;
         for(int num : nums) {
             product = product * num;
@@ -13,7 +13,7 @@ public class Product {
         for(int i = 0; i < n; i++) {
             ans[i] = product / nums[i];
         }
-        return ans; // return ans
+        return ans;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,3,4};
