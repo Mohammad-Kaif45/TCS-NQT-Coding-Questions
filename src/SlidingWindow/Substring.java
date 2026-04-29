0946b67d3d4546503a07c7dce50f7dbd11d0ec53
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-04-29 11:15:25 +0530

    Maximum length of substring without repeating characters using hashing

--- src/SlidingWindow/Substring.java
+++ src/SlidingWindow/Substring.java
@@ -3,10 +3,9 @@ package SlidingWindow;
 public class Substring {
     public static void print(String str) {
         int n = str.length();
-
         int maxLen = 0;
         for(int i = 0; i < n; i++) {
-            int[] hash = new int[256];
+            int[] hash = new int[256]; // store frequency of character
             for(int j = i; j < n; j++) {
                 if(hash[str.charAt(j)] == 1) break;
                 int len = j - i + 1;
