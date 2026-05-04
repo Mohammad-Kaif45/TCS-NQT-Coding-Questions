40eb28f1fef737a27fef6fd0bdfddc5efdc53e07
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-04 19:50:10 +0530

    Index of Peak Element in an array.

--- src/Arrays/PeakElement.java
+++ src/Arrays/PeakElement.java
@@ -2,14 +2,28 @@ package Arrays;
 
 public class PeakElement {
     public static int findIndex(int[] nums) {
-        int n = nums.length;
-        for(int i = 0; i < n; i++) {
-            // left check && right check - > true
-            if((i == 0 || nums[i - 1] < nums[i]) && (n - 1 == 0 || nums[i] > nums[i + 1])) {
-                return i;
+//        int n = nums.length;
+//        for(int i = 0; i < n; i++) {
+//            // left check && right check - > true
+//            if((i == 0 || nums[i - 1] < nums[i]) && (n - 1 == 0 || nums[i] > nums[i + 1])) {
+//                return i;
+//            }
+//        }
+//        return -1;
+
+        int low = 0, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2; // middle element
+
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;   // Rising slope → go right
+            } else {
+                high = mid;      // Falling slope → go left (mid could be peak)
             }
         }
-        return -1;
+
+        return low;
     }
     public static void main(String[] args) {
         int[] nums = {1,2,3,1};
