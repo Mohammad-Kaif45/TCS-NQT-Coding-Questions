89464f8b82b2a2db8c665a01ecf10ee10842e1c8
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-05-18 20:26:48 +0530

    Min Heap

---
+++ src/MinHeap.java
@@ -0,0 +1,16 @@
+import java.util.PriorityQueue;
+
+public class MinHeap {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        minHeap.offer(3);
+        minHeap.offer(2);
+        minHeap.offer(1);
+        minHeap.offer(4);
+        minHeap.offer(7);
+        minHeap.offer(5);
+
+        System.out.println(minHeap);
+        System.out.println(minHeap.poll());
+    }
+}
