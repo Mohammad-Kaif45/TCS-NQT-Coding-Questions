c3f3e35c3d79455d613a1b666ccf255f7cc7c278
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-06-27 12:46:31 +0530

    Ceiling and Floor problem of Binary Search Algorithm

---
+++ src/BinarySearch/Floor.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class Floor {
+    public static int search(int[] nums,int target) {
+        int n = nums.length;
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] > target) {
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,4,7,8,11,16,18,24,28};
+        int target = 19;
+        int ans = search(nums,target);
+        System.out.println(ans);
+    }
+}
+
