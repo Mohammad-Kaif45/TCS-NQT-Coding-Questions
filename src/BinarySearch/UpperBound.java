c9df5e6d25388375f04bf44f547dd2490c43c53f
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-01 22:39:36 +0530

    Upper Bound

---
+++ src/BinarySearch/UpperBound.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class UpperBound {
+    public static int upperBound(int[] arr, int x) {
+        int low = 0, high = arr.length - 1;
+        int ans = arr.length;  // Default to length if not found
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (arr[mid] > x) {
+                ans = mid;        // Store current index as potential answer
+                high = mid - 1;   // Move left
+            } else {
+                low = mid + 1;    // Move right
+            }
+        }
+        return ans;  // Return final answer
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 8, 9, 15, 19};  // Sorted array
+        int x = 9;
+
+        int ans = upperBound(arr,x);
+        System.out.println("The upper bound is the index: " + ans);
+    }
+}
