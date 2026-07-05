7cec43acf6435d1de15a0a0a5b40eb216e96e694
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-05 16:24:51 +0530

    Binary Search on 2D Array

---
+++ src/BinarySearchOn2DArray/TwoDMatrix.java
@@ -0,0 +1,38 @@
+package BinarySearchOn2DArray;
+
+public class TwoDMatrix {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+
+        int row = 0;
+        int col = matrix[0].length - 1; // start top-right
+
+        while (row < matrix.length && col >= 0) {
+            int current = matrix[row][col];
+
+            if (current == target) {
+                return true;
+            } else if (current > target) {
+                col--; // eliminate this column
+            } else {
+                row++; // eliminate this row
+            }
+        }
+
+        return false;
+    }
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 4, 7, 11, 15},
+                {2, 5, 8, 12, 19},
+                {3, 6, 9, 16, 22},
+                {10, 13, 14, 17, 24},
+                {18, 21, 23, 26, 30}
+        };
+        int target = 8;
+        boolean ans = searchMatrix(matrix,target);
+        System.out.println(ans);
+    }
+}
