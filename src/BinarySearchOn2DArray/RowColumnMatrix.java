d194626ee279407ee45b2b3f6ffdc48e96ea7622
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-07-07 20:24:20 +0530

    Update

--- src/BinarySearchOn2DArray/RowColumnMatrix.java
+++ src/BinarySearchOn2DArray/RowColumnMatrix.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class RowColumnMatrix {
     public static void main(String[] args) {
-        // 2d matrix
+        // 2d matrix input
         int[][] matrix = {
                 {-4,  9, 12},
                 {16, 18, 33},
