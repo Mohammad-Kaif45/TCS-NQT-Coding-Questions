22acc3823dc75cb0383e494ab7e64d2f0cfb1235
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-08-10 21:18:48 +0530

    Current sum array

---
+++ src/CurrSumArray.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class CurrSumArray {
+    public static int[] solve(int[] nums) {
+        int n = nums.length;
+        int curr = 0;
+        int[] ans = new int[n];
+        int idx = 0;
+        for (int num : nums) {
+            curr = curr + num;
+            ans[idx++] = curr;
+        }
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,0,2};
+        System.out.println(Arrays.toString(solve(nums)));
+    }
+}
