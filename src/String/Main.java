e1e032cbcc5731472a82e3f1dd4c1d7969fa966b
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-08-24 12:48:42 +0530

    String

---
+++ src/String/Main.java
@@ -0,0 +1,10 @@
+
+package String;
+
+public class Main {
+    public static void main(String[] args) {
+        String a = "Kaif";
+        String b = new String("Kaif");
+        System.out.println(a.equals(b));
+    }
+}
