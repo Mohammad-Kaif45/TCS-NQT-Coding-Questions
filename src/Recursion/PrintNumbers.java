9854f3865f8a3476144e6127aa9802f5242d19c0
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-08-30 12:58:04 +0530

    Recursion Level 1

--- src/Recursion/PrintNumbers.java
+++ src/Recursion/PrintNumbers.java
@@ -2,16 +2,24 @@ package Recursion;
 
 public class PrintNumbers {
     static void print(int n){
-
-        if(n==5){
-            System.out.println(5);
-            return;
-        }
+//        if(n>0){
+//            System.out.println(n);
+//            print(n-1);
+//
+//        }
+        if(n == 0) return;
         System.out.println(n);
-        print(n+1);
+        print(n-1);
 
     }
+    static void print2(int n){
+        if(n <= 10){
+            System.out.println(n);
+            print2(n+1);
+        }
+    }
     public static void main(String[] args) {
-        print(1);
+        print(10);
+        print2(1);
     }
 }
