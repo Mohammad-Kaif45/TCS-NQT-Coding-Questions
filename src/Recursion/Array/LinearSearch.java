985a729f3f1ba4da8a1be6c8422b8688eb184755
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-08-31 12:40:10 +0530

    Arrays Problem Using recursive approach

--- src/Recursion/Array/LinearSearch.java
+++ src/Recursion/Array/LinearSearch.java
@@ -1,5 +1,7 @@
 package Recursion.Array;
 
+import java.util.ArrayList;
+
 public class LinearSearch {
     public static int search(int[] nums,int target,int idx) {
         if(idx == nums.length) {
@@ -10,9 +12,25 @@ public class LinearSearch {
         }
         return search(nums,target,idx + 1);
     }
+    public static void linearSearch(int[] nums, int target, int index,
+                                    ArrayList<Integer> ans) {
+
+        if (index == nums.length) {
+            return;
+        }
+
+        if (nums[index] == target) {
+            ans.add(index);
+        }
+
+        linearSearch(nums, target, index + 1, ans);
+    }
     public static void main(String[] args) {
-        int[] nums = {3,2,1,18,9};
-        int target = 90;
+        int[] nums = {1,2,2,3,2,4,2,5,2,6};
+        int target = 2;
         System.out.println(search(nums,target,0));
+        ArrayList<Integer> ans = new ArrayList<>();
+        linearSearch(nums,target,0,ans);
+        System.out.println(ans);
     }
 }
