c1444277ab6dfed80e049c9fc7115aeb1d31b68a
Mohammad-Kaif45 <kaifmumtajansari@gmail.com>
2026-09-01 11:38:27 +0530

    Arrays Problem Using recursive approach

--- src/Recursion/Array/LinearSearch.java
+++ src/Recursion/Array/LinearSearch.java
@@ -3,34 +3,36 @@ package Recursion.Array;
 import java.util.ArrayList;
 
 public class LinearSearch {
-    public static int search(int[] nums,int target,int idx) {
-        if(idx == nums.length) {
-            return -1;
-        }
-        if (nums[idx] == target) {
-            return idx;
-        }
-        return search(nums,target,idx + 1);
-    }
-    public static void linearSearch(int[] nums, int target, int index,
+//    public static int search(int[] nums,int target,int idx) {
+//        if(idx == nums.length) {
+//            return -1;
+//        }
+//        if (nums[idx] == target) {
+//            return idx;
+//        }
+//        return search(nums,target,idx + 1);
+//    }
+    public static ArrayList<Integer> linearSearch(int[] nums, int target, int index,
                                     ArrayList<Integer> ans) {
 
         if (index == nums.length) {
-            return;
+            return ans;
         }
 
         if (nums[index] == target) {
             ans.add(index);
         }
 
-        linearSearch(nums, target, index + 1, ans);
+        return linearSearch(nums, target, index + 1, ans);
     }
     public static void main(String[] args) {
         int[] nums = {1,2,2,3,2,4,2,5,2,6};
         int target = 2;
-        System.out.println(search(nums,target,0));
-        ArrayList<Integer> ans = new ArrayList<>();
-        linearSearch(nums,target,0,ans);
-        System.out.println(ans);
+//        System.out.println(search(nums,target,0));
+//        ArrayList<Integer> ans = new ArrayList<>();
+//        linearSearch(nums,target,0,ans);
+//        System.out.println(ans)
+//      ;
+        System.out.println(linearSearch(nums,target,0,new ArrayList<>()));
     }
 }
